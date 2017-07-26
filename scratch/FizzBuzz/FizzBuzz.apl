3f25bab9dfce062eeedebd22ac2c31544bb986f8
Dhaval Dalal <dhaval.dalal@software-artisan.com>
2017-07-26 18:12:21 +0530

    Added APL version and modified Haskell Version

--- scratch/FizzBuzz/FizzBuzz.apl
+++ scratch/FizzBuzz/FizzBuzz.apl
@@ -1,3 +1,32 @@
-⍝ threesAndFives ← 3 5∘.|⍳20
-threesAndFives ← 0=3 5∘.|⍳20
-⍝ threesFivesAndFifteens ← 0=3 5 15∘.|⍳20
+input ← ⍳20
+      3∘.|input
+⍝ 1 2 0 1 2 0 1 2 0 1 2 0 1 2 0 1 2 0 1 2
+      5∘.|input
+⍝ 1 2 3 4 0 1 2 3 4 0 1 2 3 4 0 1 2 3 4 0
+      5 3∘.|input
+⍝ 1 2 3 4 0 1 2 3 4 0 1 2 3 4 0 1 2 3 4 0
+⍝ 1 2 0 1 2 0 1 2 0 1 2 0 1 2 0 1 2 0 1 2
+      0=5 3∘.|input
+⍝ 0 0 0 0 1 0 0 0 0 1 0 0 0 0 1 0 0 0 0 1
+⍝ 0 0 1 0 0 1 0 0 1 0 0 1 0 0 1 0 0 1 0 0
+      select ← 2⊥0=5 3∘.|input
+⍝ 0 0 1 0 2 1 0 0 1 2 0 1 0 0 3 0 0 1 0 2
+      indices ← ⍸select≠0
+⍝ 3 5 6 9 10 12 15 18 20
+      select[indices]
+⍝ 1 2 1 1 2 1 3 1 2
+    'Fizz' 'Buzz' 'FizzBuzz'[select[indices]]
+⍝ Fizz  Buzz  Fizz  Fizz  Buzz  Fizz  FizzBuzz  Fizz  Buzz
+
+    input[indices] ← 'Fizz' 'Buzz' 'FizzBuzz'[select[indices]]
+    input
+⍝ 1 2  Fizz  4  Buzz  Fizz  7 8  Fizz  Buzz  11  Fizz  13 14  FizzBuzz  16
+       17  Fizz  19  Buzz 
+
+input ← ⍳20
+fivesAndThrees ← 0=5 3∘.|input
+select ← 2⊥fivesAndThrees
+indices ← ⍸select≠0
+input[indices] ← 'Fizz' 'Buzz' 'FizzBuzz'[select[indices]]
+input
+
---
+++ scratch/FizzBuzz/FizzBuzz_Data_Driven_After_Apl_Realization.hs
@@ -0,0 +1,33 @@
+import Data.List
+import Data.List.Split
+import Data.Digits
+
+fizzBuzz :: Int -> [String]
+fizzBuzz n = take n result
+  where
+    -- Part 0
+    -- threes = cycle [0, 0, 1]
+    -- fives  = cycle [0, 0, 0, 0, 1]
+    -- fizzBuzz = ["", "Fizz", "Buzz", "FizzBuzz"]
+    -- result = map (fizzBuzz!!) $ zipWith (\x y -> unDigits 2 [x,y]) fives threes
+    
+    -- Part 1
+    threes = cycle [0, 0, 1]
+    fives  = cycle [0, 0, 0, 0, 1]
+    fizzBuzz = ["", "Fizz", "Buzz", "FizzBuzz"]
+    select = map (fizzBuzz!!) $ zipWith (\x y -> unDigits 2 [x,y]) fives threes
+    allIndices = map show [1..n]
+    result = zipWith (\i d -> if (d == "") then i else d) allIndices select
+    
+    -- Part 2
+    -- threes = cycle [0, 0, 1]
+    -- fives  = cycle [0, 0, 0, 0, 1]
+    -- fizzBuzz = ["", "Fizz", "Buzz", "FizzBuzz"]
+    -- select = map (fizzBuzz!!) $ zipWith (\x y -> unDigits 2 [x,y]) fives threes
+    -- allIndices = map (\i -> if (isInfixOf "3" i) then "Lucky" else i) $ map show [1..n]
+    -- result = zipWith (\i d -> if (d == "" || i == "Lucky") then i else d) allIndices select
+    
+
+main :: IO ()
+main = do
+  print $ fizzBuzz 20
