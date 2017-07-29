57860aac9845721b64379bd8271faf2de049f9ff
Dhaval Dalal <dhaval.dalal@software-artisan.com>
2017-07-30 00:28:02 +0530

    Improved it

--- scratch/FizzBuzz/FizzBuzz_Data_Driven.hs
+++ scratch/FizzBuzz/FizzBuzz_Data_Driven.hs
@@ -10,18 +10,19 @@ fizzBuzz n = take n result
     -- result = zipWith (++) threes fives
     
     -- Part 1
-    -- threes = cycle ["", "", "Fizz"]
-    -- fives  = cycle ["", "", "", "", "Buzz"]
-    -- indices = map show [1..n]
-    -- result = zipWith3 (\i t f -> max i $ t ++ f) indices threes fives
-    -- result = zipWith3 (\i t f -> if (t == "" && f == "") then i else t ++ f) indices threes fives
-    
-    -- Part 2
     threes = cycle ["", "", "Fizz"]
     fives  = cycle ["", "", "", "", "Buzz"]
-    luckies = cycle ["Lucky"]
-    indices = zipWith (\x y -> if (isInfixOf "3" x) then y else x) (map show [1..n]) luckies
-    result = zipWith3 (\i t f -> max i $ t ++ f) indices threes fives
+    indices = map show [1..]
+    select = \i t f -> if (t == "" && f == "") then i else t ++ f
+    result = zipWith3 select indices threes fives
+    -- result = zipWith3 (\i t f -> max i $ t ++ f) indices threes fives
+
+    -- Part 2
+    -- threes = cycle ["", "", "Fizz"]
+    -- fives  = cycle ["", "", "", "", "Buzz"]
+    -- luckyOrIndex = \i -> if (isInfixOf "3" $ show i) then "Lucky" else show i
+    -- select = map luckyOrIndex [1..]
+    -- result = zipWith3 (\i t f -> max i $ t ++ f) select threes fives
 
 main :: IO ()
 main = do
