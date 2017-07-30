9aaf28c342d11028ffac5ce5240cab3a1b991a55
Dhaval Dalal <dhaval.dalal@software-artisan.com>
2017-07-30 10:52:34 +0530

    Improvised

--- scratch/FizzBuzz/FizzBuzz_Data_Driven_After_Apl_Realization.hs
+++ scratch/FizzBuzz/FizzBuzz_Data_Driven_After_Apl_Realization.hs
@@ -1,7 +1,5 @@
-import Data.List (isInfixOf)
-import Data.Digits (unDigits)
+import Data.Digits (digits, unDigits)
 import Data.Time (getCurrentTime, diffUTCTime)
-
 
 fizzBuzz :: Int -> [String]
 fizzBuzz n = take n result
@@ -13,19 +11,21 @@ fizzBuzz n = take n result
     -- result = map (fizzBuzz!!) $ zipWith (\x y -> unDigits 2 [x,y]) fives threes
     
     -- Part 1
-    threes = cycle [0, 0, 1]
-    fives  = cycle [0, 0, 0, 0, 1]
-    fizzBuzz = ["", "Fizz", "Buzz", "FizzBuzz"]
-    base2 = unDigits 2
-    result = zipWith3 (\f t i -> if(base2 [f,t] == 0) then show i else fizzBuzz !! (base2 [f,t])) fives threes [1..]
-
-    -- Part 2
     -- threes = cycle [0, 0, 1]
     -- fives  = cycle [0, 0, 0, 0, 1]
-    -- fizzBuzz = ["", "Fizz", "Buzz", "FizzBuzz", "Lucky"]
-    -- indexContains3 n = isInfixOf "3" $ show n
-    -- indices = zipWith3 (\f t i -> if (indexContains3 i) then 4 else (unDigits 2 [f,t])) fives threes [1..]
-    -- result = zipWith (\idx n -> if (idx == 0) then show n else fizzBuzz!!idx) indices [1..]
+    -- fizzBuzz = ["", "Fizz", "Buzz", "FizzBuzz"]
+    -- base2 = unDigits 2
+    -- select = \f t i -> if(base2 [f,t] == 0) then show i else fizzBuzz !! (base2 [f,t])
+    -- result = zipWith3 select fives threes [1..]
+
+    -- Part 2
+    threes = cycle [0, 0, 1]
+    fives  = cycle [0, 0, 0, 0, 1]
+    fizzBuzz = ["", "Fizz", "Buzz", "FizzBuzz", "Lucky"]
+    contains3 n = elem 3 $ digits 10 n
+    select = \f t i -> if (contains3 i) then 4 else (unDigits 2 [f,t])
+    indices = zipWith3 select fives threes [1..]
+    result = zipWith (\idx n -> if (idx == 0) then show n else fizzBuzz!!idx) indices [1..]
 
 main :: IO ()
 main = do
