0e82cbd443d9e397af9ac50bf68196f384516a19
Dhaval Dalal <dhaval.dalal@software-artisan.com>
2017-08-16 17:22:14 +0530

    controlflowanddataflow

---
+++ scratch/Primes/primes_after_apl_realization.scala
@@ -0,0 +1,12 @@
+def primes(until: Int) : List[Int] = {
+  val vector = for {
+    i <- 1 to until
+    j <- 1 to until
+  } yield if (j % i == 0) 1 else 0
+
+  val matrix = vector.grouped(until).toVector.transpose
+  val summed = matrix.map(row => row.sum).toList
+  summed.zip(Stream from 1).collect { case (value, idx) if (value == 2) => idx}
+}
+
+println(primes(13))
