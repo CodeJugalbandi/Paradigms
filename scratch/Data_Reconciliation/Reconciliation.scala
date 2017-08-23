21aa36492ebe8574e085c9eda1778a6a4738e387
Dhaval Dalal <dhaval.dalal@software-artisan.com>
2017-08-23 10:54:46 +0530

    Enabled reading from csv file

--- scratch/Data_Reconciliation/Reconciliation.scala
+++ scratch/Data_Reconciliation/Reconciliation.scala
@@ -1,21 +1,30 @@
 type Row = List[String]
 type Rows = List[Row]
 
-val source = List(
-  List("TIY6M9",	"simon", "$500"),  
-  List("TIY6M0",	"jake",  "$3000"),
-  List("07003",  "wake",   "$1000"),
-  List("07006",  "sandra", "$6500"),
-  List("TIY6M9",	"simon", "$800")  
-)
 
-val mirror = List(
-  List("07003","$200"	, "wake",	  "MT951"),
-  List("TIY6M9","$800", "simon",	"MT950"),
-  List("07006","$6500",	"sandra",	"MT950"),
-  List("TIY6M9","$500", "simon",	"MT952"),
-  List("07003","$800"	, "wake",	  "MT951")
-)
+// val source = List(
+//   List("TIY6M9",  "simon", "$500"),
+//   List("TIY6M0",  "jake",  "$3000"),
+//   List("07003",  "wake",   "$1000"),
+//   List("07006",  "sandra", "$6500"),
+//   List("TIY6M9",  "simon", "$800")
+// )
+//
+// val mirror = List(
+//   List("07003","$200"  , "wake",    "MT951"),
+//   List("TIY6M9","$800", "simon",  "MT950"),
+//   List("07006","$6500",  "sandra",  "MT950"),
+//   List("TIY6M9","$500", "simon",  "MT952"),
+//   List("07003","$800"  , "wake",    "MT951")
+// )
+def dataFrom(csvFile: String) = 
+  (for {
+    line <- io.Source.fromFile(csvFile).getLines
+  } yield line.split(",").map(_.trim).toList).toList
+  
+val source = dataFrom("source.csv")
+val mirror = dataFrom("mirror.csv")
+
 
 def matches(source: Rows, mirror: Rows) = {
   val matched : List[(Row, Row)] = for {
@@ -40,6 +49,8 @@ def nonMatches(consolidatedMatches: List[(Rows, Rows)], source: Rows, mirror: Ro
   val mMatched = mirrorMatched.flatten.distinct
   (source diff sMatched, mirror diff mMatched)
 }
+
+
 
 val consolidatedMatches = matches(source, mirror)
 println("Matches...")
