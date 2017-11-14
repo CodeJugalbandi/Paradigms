847453b8ed180a827fb168fe3ec0c8799470ec1b
Dhaval Dalal <dhaval.dalal@software-artisan.com>
2017-11-14 11:01:01 +0530

    Added Prebaked code for demo

---
+++ melodies/AbstractionsAndTransparency/MarsRover/marsrover-prebaked.js
@@ -0,0 +1,37 @@
+function Vector(x, y, direction) {
+  var directions = {
+    'N' : ['W', 'E', (x,y) => [x, y+1]],
+    'E' : ['N', 'S', (x,y) => [x+1, y]],
+    'S' : ['E', 'W', (x,y) => [x, y-1]],
+    'W' : ['S', 'N', (x,y) => [x-1, y]]
+  };
+
+  this.moveForward = () => {
+    var [newX, newY] = directions[direction][2](x, y);
+    return new Vector(newX, newY, direction);
+  };
+
+  this.turnLeft = () => new Vector(x, y, directions[direction][0]);
+  this.turnRight = () => new Vector(x, y, directions[direction][1]);
+  this.toString = () => `${x} ${y} ${direction}`;
+}
+
+function MarsRover(x, y, dirString) {
+  var vector = new Vector(x, y, dirString);
+
+  var commands = {
+    'M': vector => vector.moveForward(),
+    'L': vector => vector.turnLeft(),
+    'R': vector => vector.turnRight()
+  };
+  
+  this.rove = command => {
+    vector = commands[command](vector);
+    return this;
+  }
+
+  this.toString = () => vector.toString();
+}
+
+var rover = new MarsRover(3, 3, 'E');
+console.info(rover.rove('M').rove('R').rove('M').rove('L').toString());
